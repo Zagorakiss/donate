6b4a7f21e8f56315331f04626902a0118c71d5ca
Zagora <zagorakom@gmail.com>
2016-01-07 23:22:53 +0100

    Favicons + NiceScroll

---
+++ js/donate.js
@@ -0,0 +1,11 @@
+// SCROLLBAR -----------------------------------------------------
+$(document).ready(
+
+  function() { 
+
+    $("html").niceScroll({cursorwidth:"10px",cursorborderradius:"0px",cursorcolor:"#271900",cursorborder:"1px solid #ffa500",cursoropacitymin:"0.1",hwacceleration:"false",background:"rgba(39,25,0,0.1)",hidecursordelay:"100"});
+    
+  }
+
+);
+// SCROLLBAR END -------------------------------------------------
